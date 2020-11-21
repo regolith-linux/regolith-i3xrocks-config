5149355f3dd186ffe1c74d1fa35c4e646e2b5925
Moritz Heiber <hello@heiber.im>
2020-11-21 17:39:00 +0100

    Added tests for temp script and reworked temp script routine

---
+++ tests/temp_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -Eeux -o pipefail
+
+echo ${BASH_SOURCE[0]}
+
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "${SCRIPT_DIR}"
+
+declare -a FIXTURES
+FIXTURES=( "first" "second" )
+
+for fixture in "${FIXTURES[@]}"
+do
+    PATH="$(pwd)/fixtures/temp/${fixture}:${PATH}"
+    OUTPUT=$(../scripts/temp)
+    echo "${OUTPUT}" | grep -q "40"
+done
