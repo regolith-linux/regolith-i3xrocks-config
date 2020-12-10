cb260c873db0d69c155f0e83ac2b8c7204661fbd
Moritz Heiber <github@heiber.im>
2020-12-10 10:05:07 +0100

    Rewrote net-traffic script, fixes #91 (#92)
    * Rewrote net-traffic script to be easier to maintain, added tests for it
    
    * Make sure we only read a single line from ip route
    
    * Bumping delay to 5 seconds by default, removing comments from configuration that are no longer valid

--- tests/net-traffic_test.sh
+++ tests/net-traffic_test.sh
@@ -5,6 +5,14 @@ set -Eeux -o pipefail
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "${SCRIPT_DIR}/"
 
+# Regular syntax
+[ "2" -eq "$(../scripts/net-traffic | ag -o '(?<=\>\s)(\d{1,4}|\d{1,3},?\d{0,1}?)[GMKB](?=\<)' | wc -l)" ]
+
+# up, down, total as RT
+for summary in up down total ; do
+    [ "1" -eq "$(RT=${summary} ../scripts/net-traffic | ag -o '(?<=\>\s)(\d{1,4}|\d{1,3},?\d{0,1}?)[GMKB](?=\<)' | wc -l)" ]
+done
+
 # Test the two button inputs
 button=1
 export button
