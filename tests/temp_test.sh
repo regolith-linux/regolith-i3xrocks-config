38da9e15a68f48e93e2a390dbea623e8bda2d9fe
Jesse Cooke <18191+jc00ke@users.noreply.github.com>
2022-06-26 11:42:09 -0700

    Add support to temp script for Tctl (#124)
    And run formatter.

--- tests/temp_test.sh
+++ tests/temp_test.sh
@@ -8,11 +8,10 @@ SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "${SCRIPT_DIR}"
 
 declare -a FIXTURES
-FIXTURES=( "first" "second" "third" )
+FIXTURES=("first" "second" "third" "fourth")
 
-for fixture in "${FIXTURES[@]}"
-do
-    PATH="$(pwd)/fixtures/temp/${fixture}:${PATH}"
-    OUTPUT=$(../scripts/temp)
-    echo "${OUTPUT}" | grep -q "40"
+for fixture in "${FIXTURES[@]}"; do
+  PATH="$(pwd)/fixtures/temp/${fixture}:${PATH}"
+  OUTPUT=$(../scripts/temp)
+  echo "${OUTPUT}" | grep -q "40"
 done
